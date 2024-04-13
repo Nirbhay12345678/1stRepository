dee843ffb527b20468bc8da449f5e1b15cf6f8dc
Nirbhay12345678 <140712895+Nirbhay12345678@users.noreply.github.com>
2024-04-13 11:59:41 +0530

    Add files via upload

---
+++ ByteRange.java
@@ -0,0 +1,19 @@
+class ByteRange
+
+{
+public static void main(String [] ar)
+{
+
+//byte b=197;
+//int  a=14;
+//int c1=14;
+char a= 'A';
+//byte b=a+10;
+Short s= a+10;
+
+//System.out.println(b);
+System.out.println(s);
+
+}
+
+}
